b4192ffea64418fa11725ffb1cc7e3ed6b174b5c
Markell Pogosov <szerencsbtlen404@gmail.com>
2016-12-01 19:06:55 +0200

    add simple mutation

--- app/graphs/graph_schema.rb
+++ app/graphs/graph_schema.rb
@@ -1,20 +1,50 @@
 QueryType = GraphQL::ObjectType.define do
-  p "***********************"
   name "Query"
   description "The query root for this schema"
 
   field :blog do
-    p "***********************"
     type Types::BlogType
     argument :id, !types.ID
     resolve -> (obj, args, ctx) {
-      p args
       Blog.find(args[:id])
+    }
+  end
+
+  field :all_blogs, types[Types::BlogType] do
+    resolve -> (obj, args, ctx) {
+      Blog.all
     }
   end
 end
 
+MutationType = GraphQL::ObjectType.define do
+  name "Mutation"
+
+
+  field :createBlog, field: CreateBlogMutation.field
+end
+
+CreateBlogMutation = GraphQL::Relay::Mutation.define do
+
+   name "CreateBlog"
+
+   input_field :title,   !types.String
+   input_field :content, !types.String
+
+   return_field :blog, Types::BlogType
+
+  resolve ->(obj, inputs, ctx) {
+    blog = Blog.create(title: inputs[:title], content: inputs[:content])
+
+    {
+      blog: blog,
+    }
+  }
+end
+
+
 
 GraphSchema = GraphQL::Schema.define do
   query QueryType
+  mutation MutationType
 end
