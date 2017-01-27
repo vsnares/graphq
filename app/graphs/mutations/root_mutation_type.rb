ba6bb63af4ea74851c2594d385f2c8ae22072e8e
Markell Pogosov <szerencsbtlen404@gmail.com>
2017-01-27 18:17:31 +0200

    more mutations

--- app/graphs/mutations/root_mutation_type.rb
+++ app/graphs/mutations/root_mutation_type.rb
@@ -10,5 +10,24 @@ module Mutations
         blog = Blog.create(title: args[:title], content: args[:content])
       }
     end
+
+    field :updateBlog do
+      type Types::BlogType
+      argument :id, !types.ID
+      argument :title, !types.String
+      argument :content, types.String
+      resolve ->(obj, args, ctx) {
+        Blog.find(args[:id]).update(title: args[:title], content: args[:content])
+        Blog.find(args[:id])
+      }
+    end
+
+    field :deleteBlog do
+      type Types::BlogType
+      argument :id, !types.ID
+      resolve ->(obj, args, ctx) {
+        Blog.find(args[:id]).delete
+      }
+    end
   end
 end
